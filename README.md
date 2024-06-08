143314c437415113d6bb3bc53763076e9ce88138
@luccabasto <lcbeezus@outlook.com>
2024-06-07 23:56:22 -0300

    Retirando docs inuteis

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Meu time é composto por 3 intregrantes (me incluindo):
     <li>Lucas Basto</li>
 
 <div>
-    <article>Nossa solução consiste em loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</article>
+    <article>
+    <p>Nossa solução consiste em uma plataforma onde o usuário irá acessar seu login e verificar o saldo do seus pontos. Os pontos são acumulados através da coleta de residuos (lixos) em pontos de coleta, quanto mais descarte tiver, mais pontos acumula e com isso irá trocar por cupons de desconto para ser utilizado em empresas parceiras.</p>
+    </article>
 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,19 @@ export default function HomePage() {
   return (
     <div>
       <section>
-        <h1 style={{color: variables.primaryColor}}>Hello, Lucca Basto</h1>
-        <p>Essa é a sua home---Page</p>
+        <h1 style={{color: variables.primaryColor}}>Hello, professor</h1>
+
+        <p>Desculpa o estado do projeto, diria que faltou refinamento // tempo para conseguir desenvolver nível sprint
+          espero que leve isso em consideração. 
+
+          Ao entrar na rota de login utiliza as credenciais
+          * vitorlucas@hotmail.com (Usuário)
+          * vivitor123 (Senha)
+
+          E será redirecionado para a rota exclusiva de usuários. 
+        </p>
+
+        <a href="https://github.com/luccabasto/GloabalSolution2024/tree/main">Github link</a>
         <EmailForm/>
       </section>
     </div>
